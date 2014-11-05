eaaf155ec1490c5bb3a3f6c7142b16d6687c6468
hejenstedt <l.hejenstedt@gmail.com>
2014-11-05 15:17:54 +0100

    testar Lina

--- src/memoryGame/testGit.java
+++ src/memoryGame/testGit.java
@@ -3,9 +3,9 @@ package memoryGame;
 public class testGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("ANTON!");
 
+		System.out.println("LINA");
 	}
 
 }
