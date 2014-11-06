875d6a00c1521600b4e9a113597c2fa756d24cce
skaro87 <aschnackenburg@hotmail.com>
2014-11-06 12:16:44 +0100

    Working on Grid Class

--- src/memoryGame/Grid.java
+++ src/memoryGame/Grid.java
@@ -1,20 +1,51 @@
 package memoryGame;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Grid {
 	
-	Brick[][] myGridOfBriks;
+	private int sizeX, sizeY; // size of grid
+	private Brick[][] myGridOfBriks = new Brick[sizeX][sizeY]; //grid of bricks
 
-	Grid(int sizeX,int sizeY)
+	Grid(int sizeX,int sizeY) // constructor
 	{
-		
+		this.createGrid(sizeX, sizeY); //creates grid and adds tiles to it
 	}
-	String getBrick(int x, int y)
+	
+	public void createGrid(int sizeX, int sizeY){
+		this.sizeX = sizeX; //number of rows
+		this.sizeY = sizeY; //number of columns
+		
+		int numberOfBricks = sizeX * sizeY; //total number of bricks
+		
+		ArrayList <Brick> deckOfBricks = new ArrayList <Brick>(); //list of bricks to be shuffled
+		
+		for (int i = 0; i < numberOfBricks / 2; i++){ //loop start
+			deckOfBricks.add(new Brick(""+i)); //creates two bricks with id 
+			deckOfBricks.add(new Brick(""+i));
+		} //loop end
+		
+		Collections.shuffle(deckOfBricks); // shuffles list
+		
+		int tilesAddedToGrid = 0; //number of bricks added to grid
+		
+		for (int i = 0; i < sizeX; i++){ // outer loop start
+			for (int j = 0; j < sizeY; j++){ //inner loop start
+			myGridOfBriks[i][j] = deckOfBricks.get(tilesAddedToGrid); //adds brick to grid
+			tilesAddedToGrid++; 
+			} //inner loop  end
+		} //outer loop end
+		
+	} //method createGrid end
+	
+	public String getBrick(int x, int y)
 	{
 		return null;
 		
 	}
 	
-	void showGrid()
+	public void showGrid()
 	{
 		
 	}
