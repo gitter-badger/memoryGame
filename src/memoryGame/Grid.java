362fb342d6d53a2e1ce70dea92fa18a0f787c816
m-aamir-ashfaq <amirashfaq@Amirs-MacBook-Air-2.local>
2014-11-06 10:32:27 +0100

    nowHopefully itsOK

---
+++ src/memoryGame/Grid.java
@@ -0,0 +1,21 @@
+package memoryGame;
+
+public class Grid {
+	
+	Brick[][] myGridOfBriks;
+
+	Grid(int sizeX,int sizeY)
+	{
+		
+	}
+	String getBrick(int x, int y)
+	{
+		return null;
+		
+	}
+	
+	void showGrid()
+	{
+		
+	}
+}
---
+++ src/memoryGame/cmdRunner.java
@@ -0,0 +1,10 @@
+package memoryGame;
+
+public class cmdRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
